17f02b96658f281d5a15421d5d05d4573d7e0bf2
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2023-03-20 09:25:25 +0100

    Add files via upload

---
+++ textovesoubory02/textovesoubory02/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace textovesoubory02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(86, 73);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(136, 212);
+            this.listBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "nacist";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(435, 73);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(136, 212);
+            this.listBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ textovesoubory02/textovesoubory02/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textovesoubory02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader cistcisla = new StreamReader("cisla.txt");
+            FileStream datovytok = new FileStream("cisla.dat",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            BinaryWriter zapisovac = new BinaryWriter(datovytok, Encoding.GetEncoding("windows-1250"));
+            while (!cistcisla.EndOfStream)
+            {
+                int cislo = int.Parse(cistcisla.ReadLine());
+                zapisovac.Write(cislo);
+            }
+            //zapisovac.Close();
+            BinaryReader ctenar = new BinaryReader(datovytok, Encoding.GetEncoding("windows-1250"));
+            ctenar.BaseStream.Position = 0; // kurzor na zacatek
+            while(ctenar.BaseStream.Position < ctenar.BaseStream.Length)
+            {
+                listBox1.Items.Add(ctenar.ReadInt32());
+                
+
+            }
+            ctenar.Close();
+            datovytok.Close();
+            cistcisla.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogotevreni = new OpenFileDialog();
+            dialogotevreni.InitialDirectory = Application.StartupPath;
+            if (dialogotevreni.ShowDialog() == DialogResult.OK)
+            {
+                FileStream datovytok = new FileStream("cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter zapisovac = new StreamWriter("cisilka.txt",false, Encoding.GetEncoding("windows-1250"));
+                BinaryReader ctenar = new BinaryReader(datovytok, Encoding.GetEncoding("windows-1250"));
+                while (ctenar.BaseStream.Position < ctenar.BaseStream.Length)
+                {
+                    zapisovac.WriteLine(ctenar.ReadInt32());
+                }
+                //string jmenosouboru = dialogotevreni.FileName;
+                //delej veci
+                ctenar.Close();
+                zapisovac.Close();
+                datovytok.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader read = new StreamReader("cisilka.txt");
+            
+
+            SaveFileDialog dialogulozit = new SaveFileDialog();
+            dialogulozit.InitialDirectory = Application.StartupPath;
+            if (dialogulozit.ShowDialog() == DialogResult.OK)
+            {
+                //delej veci
+                while (!read.EndOfStream)
+                {
+                    listBox2.Items.Add(read.ReadLine());
+                }
+            }
+            read.Close();
+        }
+    }
+}
